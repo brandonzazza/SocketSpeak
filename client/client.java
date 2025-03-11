4031414b3c55b89f8c0c4ab22fa513e112e2ab84
Brandon Zazza <brandonzazza@gmail.com>
2025-03-10 23:03:21 -0400

    Added label factory WIP for input fields

--- client/client.java
+++ client/client.java
@@ -1,18 +1,36 @@
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import javax.swing.*;
 
 public class client extends JFrame {
-    
+
     public client() {
         setTitle("SocketSpeak");
         setSize(400, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new FlowLayout());
+        setLayout(new GridLayout(2, 2));
+
+        JPanel ipField = field("Server IP");
+        JPanel passField = field("Password");
+
+        add(ipField);
+        add(passField);
 
         setVisible(true);
     }
 
+    private static JPanel field(String fieldName) {
+        JPanel panel = new JPanel();
+
+        JLabel label = new JLabel(fieldName);
+        label.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JTextField text = new JTextField(10);
+        text.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        panel.add(label);
+        panel.add(text);
+        return panel;
+    }
 
     public static void main(String[] args) {
         new client();
